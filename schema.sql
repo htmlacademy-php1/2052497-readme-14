3cc3575ec118fab93b02f73b922686367f284dd2
Евгений <emv2298@icloud.com>
2022-04-07 00:57:18 +0300

    База данных правки

--- schema.sql
+++ schema.sql
@@ -5,64 +5,81 @@ DEFAULT COLLATE utf8_general_ci;
 USE readme;
 
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    datatime_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    datatime_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     email VARCHAR(128) UNIQUE NOT NULL,
     login VARCHAR(64) UNIQUE NOT NULL,
-    password VARCHAR(64) UNIQUE NOT NULL,
-    avatar TINYTEXT
-);
-
-CREATE TABLE posts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    datatime_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    header TINYTEXT NOT NULL,
-    quote_author TINYTEXT,
-    text_content TEXT,
-    photo_content TINYTEXT,
-    video_content TINYTEXT,
-    link_content TINYTEXT,
-    view_count INT,
-    user_id INT, 
-    type_id INT,
-    hashtag_id INT
-);
-
-CREATE TABLE comments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    datatime_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    content TEXT,
-    user_id INT,
-    post_id INT
-);
-
-CREATE TABLE likes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    post_id INT
-);
-
-CREATE TABLE subscriptions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    follower_id INT,
-    user_id INT
-);
-
-CREATE TABLE messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    datatime_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    content  TEXT,
-    from_user_id INT,
-    to_user_id INT
-);
-
-CREATE TABLE hashtags (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name TINYTEXT
+    password VARCHAR(64) NOT NULL,
+    avatar TINYTEXT NULL
 );
 
 CREATE TABLE type_content (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    type VARCHAR(64),
-    name VARCHAR(64)
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    type VARCHAR(64) NOT NULL,
+    name VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    datatime_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    header TINYTEXT NOT NULL,
+    quote_author TINYTEXT NULL,
+    text_content TEXT NULL,
+    photo_content TINYTEXT NULL,
+    video_content TINYTEXT NULL,
+    link_content TINYTEXT NULL,
+    view_count INT NOT NULL,
+    user_id INT NOT NULL, 
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    type_id INT NOT NULL,
+    FOREIGN KEY (type_id) REFERENCES type_content (id),
+);
+
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    datatime_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    content TEXT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    post_id INT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+
+CREATE TABLE likes (
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    post_id INT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts (id),
+    PRIMARY KEY (user_id, post_id)
+);
+
+CREATE TABLE subscriptions (
+    follower_id INT NOT NULL,
+    FOREIGN KEY (follower_id) REFERENCES users (id),
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    PRIMARY KEY (follower_id, user_id)
+);
+
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    datatime_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    content  TEXT NOT NULL,
+    from_user_id INT NOT NULL,
+    FOREIGN KEY (from_user_id) REFERENCES users (id),
+    to_user_id INT NOT NULL,
+    FOREIGN KEY (to_user_id) REFERENCES users (id)
+);
+
+CREATE TABLE hashtags (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name TINYTEXT NOT NULL
+);
+
+CREATE TABLE post_hashtag (
+    post_id INT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts (id),
+    hashtag_id INT NOT NULL,
+    FOREIGN KEY (hashtag_id) REFERENCES hashtags (id),
+    PRIMARY KEY (post_id, hashtag_id)
 );
