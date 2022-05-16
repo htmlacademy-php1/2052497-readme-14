76088d54c80e8745dd37c802f6ab7e3a2cad555f
Евгений <emv2298@icloud.com>
2022-05-17 01:56:57 +0300

    Отправка эл. почты

--- init.php
+++ init.php
@@ -1,7 +1,12 @@
 <?php
+require_once 'vendor/autoload.php';
 date_default_timezone_set('Europe/Moscow');
 $con = mysqli_connect("localhost", "root", "", "readme");
 if (!$con) {
     die('Отсутствует подключение');
 };
 mysqli_set_charset($con, "utf8");
+$transport = new Swift_SmtpTransport('smtp.gmail.com', 465, "ssl");
+$transport->setUsername('info@readme.ru');
+$transport->setPassword('12345readme');
+$mailer = new Swift_Mailer($transport);
